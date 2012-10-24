2cf1439256cdcb90160e5dcf352e4ea6caa7f9f1
Suchandra Thapa <sthapa@ci.uchicago.edu>
2012-10-24 07:40:45 +0000

    Fix script generation

--- scripts/skeleton_key.py
+++ scripts/skeleton_key.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
   
   
   script_contents = "#!/bin/bash\n"
-  script_contents += "curr_dir=`cwd`"
+  script_contents += "curr_dir=`cwd`\n"
   script_contents += "ticket=\"\n%s\n\"\n" % ticket
   script_contents += "temp_directory='%s'\n" % tempfile.mktemp()
   script_contents += '''
@@ -183,6 +183,6 @@ if __name__ == '__main__':
   script_contents += "./parrot/bin/parrot_run -a ticket -i ./chirp.ticket"
   script_contents += "%s %s" % (cvmfs_arguments, xrootd_arguments)
   script_contents +=  "%s %s $@\n" % (config.get('Application', 'script'), arguments)  
-  script_contents += "cd $curr_dir"
+  script_contents += "cd $curr_dir\n"
   script_contents += "rm -fr $temp_directory"
   open('job_script.sh', 'w').write(script_contents)
