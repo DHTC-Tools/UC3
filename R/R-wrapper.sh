676320544a1028420287c119a9672b364b458ab8
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2013-04-02 14:30:06 -0500

    wrapper cleanup

--- R/R-wrapper.sh
+++ R/R-wrapper.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-# Simple R wrapper to bring in R from the UC3 CVMFS environment
+# Simple R wrapper.
 
-# looks for R folder, if not found, attempts to compile it.
-#if [ ! -d "R-2.15.1" ]; then
-#	# Untar the source
-#	tar -xvzf R-2.15.1.tar.gz
-#	pushd R-2.15.1
-#
-#	# Compile
-#	./configure --with-x=no && make 
-#
-#	# Return to our previous directory and run script
-#	popd
-#fi
-
-# Access R from the UC3 environment
 export PATH=$PATH:/cvmfs/uc3.uchicago.edu/sw/bin
 export LD_LIBRARY_PATH=/cvmfs/uc3.uchicago.edu/sw/lib
 
